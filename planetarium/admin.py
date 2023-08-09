a2427ef4ff7573bdfa771e265c0bf7c9cbb5dcd1
Denys Seliverstov <catsperr322@gmail.com>
2023-08-09 15:50:11 +0300

    Creating models ShowTheme,                                 Reservation,                                 PlanetariumDome,                                 Ticket,                                 AstronomyShow,                                 ShowSession, register them in admin.py

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (ShowTheme,
+                                Reservation,
+                                PlanetariumDome,
+                                Ticket,
+                                AstronomyShow,
+                                ShowSession)
+
+admin.site.register(ShowTheme)
+admin.site.register(Reservation)
+admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+admin.site.register(Ticket)
+admin.site.register(ShowSession)
--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,57 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name="reservations")
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255,
+                             unique=True)
+    description = models.TextField(null=True,
+                                   blank=True)
+    show_themes = models.ManyToManyField(ShowTheme,
+                                         related_name="astronomy_shows")
+
+    class Meta:
+        ordering = ["title"]
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow,
+                                       on_delete=models.CASCADE,
+                                       related_name="show_sessions")
+    planetarium_dome = models.ForeignKey(PlanetariumDome,
+                                         on_delete=models.CASCADE,
+                                         related_name="show_sessions")
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession,
+                                     on_delete=models.CASCADE,
+                                     related_name="tickets")
+    reservation = models.ForeignKey(Reservation,
+                                    on_delete=models.CASCADE,
+                                    related_name="tickets")
